d42bf011de9d85510432be7bf59becc23db6abe0
Paul Oliver <ollie@flumeland.co.uk>
2013-03-21 15:58:40 +0000

    New Version using Codegear

---
+++ Projects/LocationClass(Array).cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string.h>
+#include <Location.h>
+#include <Node.H>
+
+using namespace std;
+
+class Locations {
+private:
+	node *locations[10]; // database array
+	public:
+	Locations();
+	void loadLocation(Location refNum) {
+	private:
+		Location locations[10];
+
+	void loadLocations(Location refNum) {
+
+	}
+};
+
---
+++ Projects/Menu.h
@@ -0,0 +1,17 @@
+#ifndef Menu_H
+#define Menu_H
+
+using namespace std;
+
+class Menu {
+public:
+	Menu();
+
+    void menuOptions(int &choice);
+    void addRecord();
+    void administerRecord();
+    void findDistance();
+
+};
+
+#endif
